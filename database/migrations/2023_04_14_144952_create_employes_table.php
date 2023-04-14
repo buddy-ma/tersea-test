c011e702c231e2dce2a5b61f63b9a7a2b2426976
buddy <info@buddy.ma>
2023-04-14 15:08:52 +0000

    define models and migrations

---
+++ database/migrations/2023_04_14_144952_create_employes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained();
+            $table->string('fullname');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('address');
+            $table->date('date_naissance');
+            $table->string('password');
+            $table->tinyInteger('status')->default(1);
+            $table->tinyInteger('isFirstTime')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employes');
+    }
+};
